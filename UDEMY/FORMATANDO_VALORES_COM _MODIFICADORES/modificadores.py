4354278bc56f72d17a7524ed999ac49fccea1924
RONALDO <familiaberohi@gmail.com>
2022-05-01 11:24:21 -0300

    Trabalhando com modificadores utilizando Fstrings

---
+++ UDEMY/FORMATANDO_VALORES_COM _MODIFICADORES/modificadores.py
@@ -0,0 +1,26 @@
+"""
+:s - Texto (strings)
+:d - Inteiros (int)
+:f - Números de ponto flutuante (float)
+:.(NÚMERO)f - Quantidade de casas decimais (float) :.1f
+:(CARACTERE)(> ou < ^)(QUANTIDADE)(TIPO - s,d ou f)
+"""
+
+num_1 = 10
+num_2 = 3
+#divisao = num_1 / num_2
+#print('{:.2f}'.format(divisao))
+#print(f'{divisao:.2f}')
+
+#nome = 'ronaldo brisa'
+#print(f'{nome:s}')
+
+# > - Esquerda
+# print(f'{num_1:0>10}')
+
+# < - Direita
+# print(f'{num_1:0<10}')
+
+# ^ - Centro
+# print(f'{num_1:0^10}')
+
